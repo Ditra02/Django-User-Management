1ee422c66b5536553ad502cd20abdcd3af2fc485
Asyafa Ditra <asyafaditra@gmail.com>
2023-11-26 20:36:01 +0700

    Django user registration and sign in

--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
+
+
 def home(request):
     return render(request=request, template_name='main/home.html')
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request=request, user=user)
+            return redirect('/home')
+
+    else:
+        form = RegisterForm()
+
+    return render(request=request, template_name='registration/sign_up.html', context={'form': form})
