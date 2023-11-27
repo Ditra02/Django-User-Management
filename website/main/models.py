98d403692e1a6e8d4825aeadbad6d4a03f6c78ae
Asyafa Ditra <asyafaditra@gmail.com>
2023-11-27 20:21:39 +0700

    create a new model, create a form, view, and template for post

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    # on delete : if the user account deleted then the post is also deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    # auto_now_add : add timestamp when post get created
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # auto_now :  whenever the post get updated it'll safe the timestamp
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title + "\n" + self.description
+
--- website/main/views.py
+++ website/main/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required(login_url="/login")
 def home(request):
     return render(request=request, template_name='main/home.html')
+
+
+@login_required(login_url="/login")
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        
+        if form.is_valid():
+            # prevent the post saved in the database
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+        
+    return render(request=request, template_name='main/create_post.html', context={'form':form})
+
 
 
 def sign_up(request):
