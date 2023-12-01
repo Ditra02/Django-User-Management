ff4c8deae77f7beaee2d39f49a8bf875f3ea129d
Asyafa Ditra <asyafaditra@gmail.com>
2023-12-01 13:54:49 +0700

    Revert "secure confidental info by .env"
    This reverts commit a24e69665b9e274249c5c98cc7de96951d543b6c.

--- website/website/settings.py
+++ website/website/settings.py
@@ -16,7 +16,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / 'secret/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -91,8 +90,8 @@ DATABASES = {
         
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
-        'USER': str(os.getenv('DATABASES_HOST_USER')),
-        'PASSWORD': str(os.getenv('DATABASES_HOST_PASSWORD')),
+        'USER': 'postgres',
+        'PASSWORD': '*3a1agGbCEb-GFFAEeFbb3Gg*beAA1Cg',
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '50346',
     }
