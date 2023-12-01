a24e69665b9e274249c5c98cc7de96951d543b6c
Asyafa Ditra <asyafaditra@gmail.com>
2023-12-01 13:49:30 +0700

    secure confidental info by .env

--- website/website/settings.py
+++ website/website/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / 'secret/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -89,8 +91,8 @@ DATABASES = {
         
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': '*3a1agGbCEb-GFFAEeFbb3Gg*beAA1Cg',
+        'USER': str(os.getenv('DATABASES_HOST_USER')),
+        'PASSWORD': str(os.getenv('DATABASES_HOST_PASSWORD')),
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '50346',
     }
